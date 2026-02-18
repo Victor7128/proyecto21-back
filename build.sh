2b347c42530b6afe9ebbe72338a732bef15c594e
Victor <tuestareynavictorhugo@gmail.com>
2026-02-18 00:51:47 -0500

    fix. build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
 
-curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor -o /usr/share/keyrings/microsoft-prod.gpg
+curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor -o /tmp/microsoft-prod.gpg
+cp /tmp/microsoft-prod.gpg /etc/apt/trusted.gpg.d/microsoft-prod.gpg
 
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft-prod.gpg] https://packages.microsoft.com/ubuntu/22.04/prod jammy main" | tee /etc/apt/sources.list.d/mssql-release.list
+echo "deb [arch=amd64] https://packages.microsoft.com/ubuntu/22.04/prod jammy main" | tee /etc/apt/sources.list.d/mssql-release.list
 
 apt-get update
 ACCEPT_EULA=Y apt-get install -y msodbcsql18 unixodbc-dev
