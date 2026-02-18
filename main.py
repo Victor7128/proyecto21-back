eb512eda1cd9dc966671518a1b3efd2dcb2f5dd8
Victor <tuestareynavictorhugo@gmail.com>
2026-02-18 16:40:08 -0500

    fix: vercel front url

--- main.py
+++ main.py
@@ -25,7 +25,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],   # Ajustar en producción
+    allow_origins=[
+        "http://localhost:3000",
+        "https://proyecto21-front.vercel.app/",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
